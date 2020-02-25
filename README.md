9ffc8d2800196229d3a6509390dae627de806ab6
wkexinw <hi_fly_@163.com>
2020-02-25 17:06:45 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+#### simpleMSSpeech
+A simple console application for MS Speech TTS.
+
+
+#### REQUIRED
+windows 10 x86_64
+Visual Studio 2017
+
+###### MicrosoftSpeechPlatformSDK_x64
+The Microsoft Speech Platform SDK.
+
+###### MSSpeech_TTS_ja-JP_Haruka
+
+Voice package for jpanese language. For other language ,see https://www.microsoft.com/en-us/download/details.aspx?id=27224.
+
+#### INSTALLATION
+
+step 1：Install MicrosoftSpeechPlatformSDK_x64.msi
+
+step 2: Install MSSpeech_TTS_ja-JP_Haruka.msi (For jpanese language)
+
+step 3: Install runtime witch path like X:\Program Files\Microsoft SDKs\Speech\v11.0\Redist\SpeechPlatformRuntime.msi
+
+step 4: Press win + r to run 'ms-settings:easeofaccess-narrator' to select 'Narrator' for your windows
+
+#### QUESITION
+If you have any quesiton ,leave a message,thank you.
+
---
+++ simpleMSSpeech/simpleMSSpeech.cpp
@@ -0,0 +1,58 @@
+﻿
+#include "pch.h"
+#include <Windows.h>
+#include <string>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <sapi.h>
+using namespace std;
+
+int main()
+{
+	ifstream input("./speech.txt", ios::in);
+	stringstream buffer;
+	buffer << input.rdbuf();
+	string contents(buffer.str());
+	/*
+	language id
+	411		jpanese
+	809		chinese
+	409		english
+	*/
+	string speech= "<VOICE REQUIRED='LANGUAGE=411' /><rate absspeed='-2' />" + contents;
+
+	/*
+		#define CP_ACP                    0           // default to ANSI code page
+		#define CP_OEMCP                  1           // default to OEM  code page
+		#define CP_MACCP                  2           // default to MAC  code page
+		#define CP_THREAD_ACP             3           // current thread's ANSI code page
+		#define CP_SYMBOL                 42          // SYMBOL translations
+
+		#define CP_UTF7                   65000       // UTF-7 translation
+		#define CP_UTF8                   65001       // UTF-8 translation
+	*/
+	int nLen = MultiByteToWideChar(CP_UTF8, MB_PRECOMPOSED, speech.c_str(), -1, NULL, 0);
+	wchar_t * wc = new wchar_t[nLen];
+	MultiByteToWideChar(CP_UTF8, MB_PRECOMPOSED, speech.c_str(), -1, wc, nLen);
+
+	ISpVoice * pVoice = NULL;
+
+	//COM initial   
+	if (FAILED(::CoInitialize(NULL)))
+		return FALSE;
+
+	//get ISpVoice interface   
+	HRESULT hr = CoCreateInstance(CLSID_SpVoice, NULL, CLSCTX_ALL, IID_ISpVoice, (void **)&pVoice);
+	if (SUCCEEDED(hr))
+	{
+		hr = pVoice->Speak(wc, SPF_DEFAULT | SPF_IS_XML, NULL);
+		pVoice->Release();
+		pVoice = NULL;
+	}
+
+	return 0;
+}
+
